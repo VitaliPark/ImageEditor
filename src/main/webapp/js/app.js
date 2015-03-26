99942299661f303f9e96ffa456a316e15f873492
Vitali Park <vitalipark@gmail.com>
2015-03-26 13:54:57 +0300

    Image display on show added

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('imageEditor', ['ui.router', 'flow']);
+var app = angular.module('imageEditor', ['ui.router']);
 
 
 app.config(function ($stateProvider, $urlRouterProvider) {
@@ -29,12 +29,11 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 app.directive('fileModel', ['$parse', function ($parse) {
     return {
         restrict: 'A',
-        link: function(scope, element, attrs) {
+        link: function (scope, element, attrs) {
             var model = $parse(attrs.fileModel);
             var modelSetter = model.assign;
-
-            element.bind('change', function(){
-                scope.$apply(function(){
+            element.bind('change', function () {
+                scope.$apply(function () {
                     modelSetter(scope, element[0].files[0]);
                 });
             });
@@ -42,9 +41,43 @@ app.directive('fileModel', ['$parse', function ($parse) {
     };
 }]);
 
+app.directive('imageBind', ['$window', function($window) {
+    var helper = {
+        support: !!($window.FileReader),
+        isFile: function(item) {
+            return angular.isObject(item) && item instanceof $window.File;
+        },
+        isImage: function(file) {
+            var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+            return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+        }
+    };
+
+    return {
+        restrict: 'A',
+        scope: {
+            file: '=imageBind'
+        },
+        link: function(scope, element, attributes) {
+            //if (!helper.support) return;
+            var reader = new FileReader();
+            reader.onload = onLoadFile;
+            scope.$watch('file', function(file) {
+                if (!helper.isFile(scope.file)) return;
+                if (!helper.isImage(scope.file)) return;
+                reader.readAsDataURL(file);
+            });
+
+            function onLoadFile(event) {
+                element[0].src = event.target.result;
+            }
+        }
+    };
+}]);
+
 app.service('fileUpload', ['$http', function ($http) {
 
-    this.uploadFileToUrl = function(leftImage, rightImage, uploadUrl){
+    this.uploadFileToUrl = function (leftImage, rightImage, uploadUrl) {
         var fd = new FormData();
         fd.append('leftImage', leftImage);
         fd.append('rightImage', rightImage);
@@ -52,9 +85,9 @@ app.service('fileUpload', ['$http', function ($http) {
             transformRequest: angular.identity,
             headers: {'Content-Type': undefined}
         })
-            .success(function(){
+            .success(function () {
             })
-            .error(function(){
+            .error(function () {
             });
     }
 }]);
--- src/main/webapp/js/controller/anaglyph.js
+++ src/main/webapp/js/controller/anaglyph.js
@@ -2,12 +2,10 @@
  * Created by vitaly.pak on 20.03.2015.
  */
 app.controller('AnaglyphController', function ($scope, fileUpload) {
-    $scope.left = {};
-    $scope.right = {};
+    $scope.leftImage = {};
+    $scope.rightImage = {};
 
-    this.message = "Hello world";
-
-    $scope.uploadFile = function(){
+    $scope.uploadFile = function () {
         var leftImage = $scope.leftImage;
         var rightImage = $scope.rightImage;
         console.log('file is ' + JSON.stringify(leftImage));
